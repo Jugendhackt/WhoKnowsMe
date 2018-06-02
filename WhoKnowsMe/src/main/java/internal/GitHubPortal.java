e78c94a6f11c97aed40e105e76d45a35105447f7
asqiir <asqiir@blaupost.de>
2018-06-02 22:46:43 +0200

    cleanup conflicts (pom.xml)

--- WhoKnowsMe/src/main/java/internal/GitHubPortal.java
+++ WhoKnowsMe/src/main/java/internal/GitHubPortal.java
@@ -5,54 +5,6 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 public class GitHubPortal extends Portal {
-	Portal singlePortal = new GitHubPortal();
-
-	@Override
-	public Portal getInstance() {
-		return singlePortal;
-	}
-
-	@Override
-	public boolean hasAccount(String accountName) throws Exception {
-		String url = "https://github.com/" + accountName + "/";
-		Document html;
-		try {
-			html = Jsoup.connect(url).get();
-		} catch (IOException e) {
-			throw new Exception("problem connecting to the webside" + url + e);
-		}
-		String htmlString = html.toString();
-		boolean member;
-		if(htmlString.contains("Page not found")) {	
-			member=false;
-		}
-		else
-		{
-			member=true;
-		}
-				
-		return member;
-	}
-
-	@Override
-	public String getUrl(String accountName) {
-		return "https://github.com/" + accountName + "/";
-	}
-
-	@Override
-	public String getPortalName() {
-		return "GitHub";
-	}
-
-}
-=======
-package internal;
-import java.io.IOException;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-
-public class GitHubPortal extends Portal {
 
 	public GitHubPortal() {
 		super("https://github.com/", "GitHub");
