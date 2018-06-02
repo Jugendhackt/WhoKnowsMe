d4a492298e6ffebbeaf936be3accd9df5dbd6dfe
asqiir <asqiir@blaupost.de>
2018-06-03 00:14:51 +0200

    replaced discouraged Assert.assertIs by assertTrue(x.equals(y))

--- WhoKnowsMe/src/test/java/main/test/GitHubPortalTest.java
+++ WhoKnowsMe/src/test/java/main/test/GitHubPortalTest.java
@@ -1,8 +1,11 @@
 package main.test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 import internal.GitHubPortal;
-import junit.framework.Assert;
+//import junit.framework.Assert;
 
 public class GitHubPortalTest {
 	@Test
@@ -12,14 +15,16 @@ public class GitHubPortalTest {
 		String url = portal.getUrl();
 		
 		//test, whether the url is right
-		Assert.assertEquals("https://github.com/jugendhackt/",url);
+		//Assert.assertEquals("https://github.com/jugendhackt/",url);
+		assertTrue(url.equals("https://github.com/jugendhackt/"));
 		
 	}
 	@Test
 	public void testGetPortalName () {
 		GitHubPortal portal = new GitHubPortal(""); //empty, we need no username
 		String portalName = portal.getPortalName();
-		Assert.assertEquals("GitHub", portalName);
+		//Assert.assertEquals("GitHub", portalName);
+		assertTrue(portalName.equals("GitHub"));
 	}
 	
 	@Test
@@ -31,7 +36,7 @@ public class GitHubPortalTest {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		Assert.assertTrue(member);
+		assertTrue(member);
 		
 		//evtl: in neuen Test auslagern (für error testen)
 		//need a second, portal have to know username, in cause of specific url (including username)
@@ -41,7 +46,7 @@ public class GitHubPortalTest {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		Assert.assertFalse(member);
+		assertFalse(member);
 	}
 
 }
