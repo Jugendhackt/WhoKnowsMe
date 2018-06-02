37b03ce04607e4486449f53c07a86ad1c7b390e0
asqiir <asqiir@blaupost.de>
2018-06-02 12:14:38 +0200

    dummy created

---
+++ WhoKnowsMe/backend/Account.java
@@ -0,0 +1,4 @@
+
+public class Account {
+	private String url;
+}
--- WhoKnowsMe/backend/BackendInterface.java
+++ WhoKnowsMe/backend/BackendInterface.java
@@ -1,4 +1,12 @@
+import java.util.*;
 
 public class BackendInterface {
+	/* This is the interface that takes user input and searchs for
+	 * accounts that are named similar */
 
+	private BackendInterface() {} //no instances can be created
+	
+	public List<Account> findAccounts(String input) {
+		return null;
+	}
 }
