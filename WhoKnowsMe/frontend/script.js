1f31535f271ed1a8562299f0bafa030eb8324028
numten <numten.j@gmail.com>
2018-06-02 22:45:49 +0200

    Mit Enter suchen, bessere Namensgebung der Funktionen.

--- WhoKnowsMe/frontend/script.js
+++ WhoKnowsMe/frontend/script.js
@@ -1,4 +1,10 @@
-function sayHello() {
-    var createnode = document.getElementById("bname").value;
-    document.getElementById("divId").innerHTML = createnode;
+function searchStarter() {
+    var username = document.getElementById("bname").value;
+    document.getElementById("divId").innerHTML = username;
 }
+function keyEnterPress(event){
+    if(event.keyCode == 13){
+        searchStarter();
+    }
+}
+window.addEventListener("keydown", keyEnterPress, false);
