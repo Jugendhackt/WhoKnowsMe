9e63684b4ff5910b69be7bae33b57c3ef5fb732b
numten <numten.j@gmail.com>
2018-06-03 09:51:10 +0200

    Lade Animation

--- WhoKnowsMe/WebContent/script.js
+++ WhoKnowsMe/WebContent/script.js
@@ -1,6 +1,11 @@
 function searchStarter() {
     var username = document.getElementById("bname").value;
     document.getElementById("divId").innerHTML = username;
+
+    var reloading = document.createElement("div")
+    reloading.id = "divreloading"
+    document.getElementById("searchButton").innerHTML = "";
+    document.getElementById("searchButton").appendChild(reloading);
 }
 function keyEnterPress(event){
     if(event.keyCode == 13){
