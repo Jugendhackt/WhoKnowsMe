26f6888c1f5b8651a76905c2f9e9c78346da24a2
Wutchilli <lucas@frantzioch.de>
2018-06-03 12:48:35 +0200

    addet websides 1/?

--- WhoKnowsMe/src/main/java/internal/PlainTextPortal.java
+++ WhoKnowsMe/src/main/java/internal/PlainTextPortal.java
@@ -33,6 +33,22 @@ public class PlainTextPortal extends Portal {
 	}
 	
 	public static final String[][] PLAIN_TEXT_URLS = {
-			{"https://github.com/USER/","GitHub","Page not found"}
+			{"https://github.com/USER/","GitHub","Page not found"},
+			{"https://twitter.com/USER/","Twitter","Twitter / ?"},
+			{"https://www.instagram.com/USER/","Instagram","Seite nicht gefunden &bull; Instagram"},
+			{"https://www.deviantart.com/USER/","Deviantart","Browse  on DeviantArt"},
+			{"https://pixabay.com/users/USER/","Pixabay","Error 404"},
+			{"https://www.fanfiktion.de/u/USER","Fanfiktion.de","Benutzerprofil nicht gefunden | FanFiktion.de"},
+			{"https://www.wattpad.com/user/USER","Wattpad","BenutzerIn nicht gefunden - Wattpad"},
+			{"https://USER.slack.com/","Slack","Es ist ein Fehler aufgetreten ... | Slack"},
+			{"https://discuss.codecademy.com/u/USER/","Codecademy","Codecademy Discuss"},
+			{"https://gitlab.com/","Gitlab","Sign in · GitLab"},
+			{"https://www.youtube.com/user/USER","YouTube","YouTube"},
+			{"https://www.gutefrage.net/nutzer/USER","Gutefrage","Gutefrage - die größte deutschsprachige Frage-Antwort-Plattform"},
+			{"https://soundcloud.com/USER","Soundcloud","SoundCloud - Hear the world’s sounds"},
+			{"https://disqus.com/by/USER","Disqus","Page not found (404) - Disqus"}
+			
+				
+				
 	};
 }
