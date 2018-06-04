9983969f8987459e1ec70371b53ad9819925b3f4
asqiir <asqiir@blaupost.de>
2018-06-04 18:50:55 +0200

    replaced Jsoup code in Portal.getWebsitesHead(url) by code from tutorial: https://charlesling.wordpress.com/2008/05/15/get-html-title-tag-using-java-htmldocument-library/

--- WhoKnowsMe/src/main/java/internal/Portal.java
+++ WhoKnowsMe/src/main/java/internal/Portal.java
@@ -1,9 +1,12 @@
 package internal;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.*;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
+import javax.swing.text.html.HTMLDocument;
+//import org.jsoup.Jsoup;
+//import org.jsoup.nodes.Document;
+import javax.swing.text.html.HTMLEditorKit;
 
 public abstract class Portal {
 	private String url;
@@ -25,8 +28,20 @@ public abstract class Portal {
 	}
 	
 	protected static String getWebsitesTitle(String url) throws IOException {
-		Document html;
-		html = Jsoup.connect(url).get();
-		return html.toString();
+//		Document html;
+//		html = Jsoup.connect(url).get();
+//		return html.toString();
+		
+		URL urlObject = new URL(url);
+		URLConnection connection = urlObject.openConnection();
+		InputStream is = connection.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+
+		HTMLEditorKit htmlKit = new HTMLEditorKit();
+
+		//Only start from body
+		HTMLDocument htmlDoc = (HTMLDocument) htmlKit.createDefaultDocument();
+		return (String) htmlDoc.getProperty(HTMLDocument.TitleProperty);
 	}
 }
--- WhoKnowsMe/src/main/java/main/AccountService.java
+++ WhoKnowsMe/src/main/java/main/AccountService.java
@@ -31,7 +31,6 @@ public class AccountService {
 					allAccounts.add(new Account(p,input));
 				}
 			} catch (Exception e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
