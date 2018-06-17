98a9506111b29716adad1438bbc1c9f1691889cb
asqiir <asqiir@blaupost.de>
2018-06-17 16:47:30 +0200

    base structure of script.js in test_environment set

---
+++ test_environment/script.js
@@ -0,0 +1,22 @@
+
+
+
+//this function will run when button #submit is clicked
+jQuery('#submit').click(function() {
+	//1) output mit Ladebalken ersetzen (optional)
+	//2) Eingabe auslesen //wenn eingabe leer, hier abbrechen (aber ladebalken wieder entfernen)
+	//3) Anfrage senden (findAccounts/EINGABE)
+	//4) antwort (json) in liste umformen
+	//4) output durch Liste ersetzen
+});
+
+
+function createList(json) {
+	//take the json,
+	//return somthing like this structur (but as nodes, not as string)
+
+	//<ul>
+	// <li><a href="URL">account</a> on PORTAL_NAME</li>
+	// //mehr list-elemente
+	//</ul>
+}
