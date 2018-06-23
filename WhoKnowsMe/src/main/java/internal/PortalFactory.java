19bae4aa64a966284b1e6d746b992bd0f2539c23
asqiir <asqiir@blaupost.de>
2018-06-23 22:52:02 +0200

    created portalfactory (unfinished)

--- WhoKnowsMe/src/main/java/internal/PortalFactory.java
+++ WhoKnowsMe/src/main/java/internal/PortalFactory.java
@@ -1,17 +1,41 @@
 package internal;
 
+import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import jdk.nashorn.internal.parser.JSONParser;
+
 public class PortalFactory {
+	private static List<Portal> allPortals = new ArrayList<Portal>();
+	private static final String PLAIN_TEXT_URLS_JSON_FILE = "plainUrls.json";
+	
+	
 	public static List<Portal> createPortals(String accountName) {
-		List<Portal> allPortals = new ArrayList<Portal>();
-		
-		//add here any
-		for(String[] inputSite:PlainTextPortal.PLAIN_TEXT_URLS) {
-			allPortals.add(new PlainTextPortal(inputSite[0],accountName, inputSite[1], inputSite[2]));
+		if(allPortals.isEmpty()) {
+	
+			//add here any
+			createPlainTextPortals(accountName, createJSON(PLAIN_TEXT_URLS_JSON_FILE));			
 		}
-		
+			
 		return allPortals;
+	}
+	
+	private static JSONArray createJSON(String fileName) { //filename is in folder resources
+		//TODO
+		return null;
+	}
+	
+	private static void createPlainTextPortals(String accountName, JSONArray json) {
+		
+		
+		for(Object obj:json) {
+			JSONObject jsObj = (JSONObject) obj; //does this work?
+			allPortals.add(new PlainTextPortal(jsObj.getString("generalUrl"),accountName, jsObj.getString(key)[1], inputSite[2]));
+		}
 	}
 }
